936ea35d94491f569debe7d75ac979edfc5164a6
pointpu <pointpu@prorobotech.ru>
2026-07-05 00:24:37 +0300

    fix(iam): UserTokenService authz → dedicated token_admin relation (least-privilege)
    v_update/v_list на iam_user конфлейтили token-management с self-edit идентити: дать
    пользователю self-service токенов через v_update = молча дать self-edit user-объекта.
    Выделенная relation token_admin (= subject or v_update: self ИЛИ кто-может-обновлять)
    разводит их — self-service токенов БЕЗ права редактировать user. Verified live FGA:
    regular-user token_admin@self=True, v_update@self=False.

--- proto/gen/go/kacho/cloud/iam/v1/user_token_service.pb.go
+++ proto/gen/go/kacho/cloud/iam/v1/user_token_service.pb.go
@@ -553,14 +553,14 @@ const file_kacho_cloud_iam_v1_user_token_service_proto_rawDesc = "" +
 	"revoked_at\x18\x02 \x01(\v2\x1a.google.protobuf.TimestampR\trevokedAt\"M\n" +
 	"\x17RevokeUserTokenMetadata\x12\x17\n" +
 	"\auser_id\x18\x01 \x01(\tR\x06userId\x12\x19\n" +
-	"\btoken_id\x18\x02 \x01(\tR\atokenId2\xe2\x05\n" +
-	"\x10UserTokenService\x12\xfb\x01\n" +
-	"\x05Issue\x12).kacho.cloud.iam.v1.IssueUserTokenRequest\x1a .kacho.cloud.operation.Operation\"\xa4\x01\x8a\xb5\x18\x1biam.issue_user_tokens.issue\x92\xb5\x18\bv_update\x9a\xb5\x18\x13\n" +
+	"\btoken_id\x18\x02 \x01(\tR\atokenId2\xed\x05\n" +
+	"\x10UserTokenService\x12\xfe\x01\n" +
+	"\x05Issue\x12).kacho.cloud.iam.v1.IssueUserTokenRequest\x1a .kacho.cloud.operation.Operation\"\xa7\x01\x8a\xb5\x18\x1biam.issue_user_tokens.issue\x92\xb5\x18\vtoken_admin\x9a\xb5\x18\x13\n" +
 	"\biam_user\x12\auser_id\xa2\xb5\x18\x012\xb2\xd2*0\n" +
-	"\x16IssueUserTokenMetadata\x12\x16IssueUserTokenResponse\x82\xd3\xe4\x93\x02#:\x01*\"\x1e/iam/v1/users/{user_id}/tokens\x12\xc3\x01\n" +
-	"\x04List\x12).kacho.cloud.iam.v1.ListUserTokensRequest\x1a*.kacho.cloud.iam.v1.ListUserTokensResponse\"d\x8a\xb5\x18\x14iam.user_tokens.list\x92\xb5\x18\x06v_list\x9a\xb5\x18\x13\n" +
-	"\biam_user\x12\auser_id\xa2\xb5\x18\x012\x82\xd3\xe4\x93\x02 \x12\x1e/iam/v1/users/{user_id}/tokens\x12\x89\x02\n" +
-	"\x06Revoke\x12*.kacho.cloud.iam.v1.RevokeUserTokenRequest\x1a .kacho.cloud.operation.Operation\"\xb0\x01\x8a\xb5\x18\x1diam.revoke_user_tokens.revoke\x92\xb5\x18\bv_update\x9a\xb5\x18\x13\n" +
+	"\x16IssueUserTokenMetadata\x12\x16IssueUserTokenResponse\x82\xd3\xe4\x93\x02#:\x01*\"\x1e/iam/v1/users/{user_id}/tokens\x12\xc8\x01\n" +
+	"\x04List\x12).kacho.cloud.iam.v1.ListUserTokensRequest\x1a*.kacho.cloud.iam.v1.ListUserTokensResponse\"i\x8a\xb5\x18\x14iam.user_tokens.list\x92\xb5\x18\vtoken_admin\x9a\xb5\x18\x13\n" +
+	"\biam_user\x12\auser_id\xa2\xb5\x18\x012\x82\xd3\xe4\x93\x02 \x12\x1e/iam/v1/users/{user_id}/tokens\x12\x8c\x02\n" +
+	"\x06Revoke\x12*.kacho.cloud.iam.v1.RevokeUserTokenRequest\x1a .kacho.cloud.operation.Operation\"\xb3\x01\x8a\xb5\x18\x1diam.revoke_user_tokens.revoke\x92\xb5\x18\vtoken_admin\x9a\xb5\x18\x13\n" +
 	"\biam_user\x12\auser_id\xa2\xb5\x18\x012\xb2\xd2*2\n" +
 	"\x17RevokeUserTokenMetadata\x12\x17RevokeUserTokenResponse\x82\xd3\xe4\x93\x02+*)/iam/v1/users/{user_id}/tokens/{token_id}BIZGgithub.com/PRO-Robotech/kacho-iam/proto/gen/go/kacho/cloud/iam/v1;iamv1b\x06proto3"
 
